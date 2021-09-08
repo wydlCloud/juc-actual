b96d3e7e8cd189a5733decb534baa28a9f783e25
yunze <yunze.wydl@raycloud.com>
2021-09-09 00:13:52 +0800

    根据业务场景，组合使用

--- src/main/java/com/wy/learn/completeFuture/demo/CompletionStageAcceptDemo.java
+++ src/main/java/com/wy/learn/completeFuture/demo/CompletionStageAcceptDemo.java
@@ -15,10 +15,7 @@ public class CompletionStageAcceptDemo {
 
     public static void main(String[] args) throws InterruptedException {
 
-        //首先通过 supplyAsync() 启动一个异步流程，之后是两个串行操作，整体看起来还是挺简单的。
-        // 不过，虽然这是一个异步流程，但任务①②③却是串行执行的，
-        // ②依赖①的执行结果，
-        // ③依赖②的执行结果。
+      // 也可以thenApply和thenAccept组合使用，根据具体的业务场景
 
         CompletableFuture<Void> ft = CompletableFuture.supplyAsync(() -> {
             return "hello world";
@@ -27,6 +24,18 @@ public class CompletionStageAcceptDemo {
                     // 在此可以继续处理业务，但是入参是空的，因为上面的步骤是不能用的，而且外部的参数也传不进来，比如上面定义的静态变量
                     System.out.println("123");
                 });
+
+
+        CompletableFuture<String> ft1 = CompletableFuture.supplyAsync(() -> {
+            return "hello world";
+        }).thenAccept(s -> System.out.println(s))
+                .thenApply(ss -> {
+                    // 在此可以继续处理业务，但是入参是空的，因为上面的步骤是不能用的，而且外部的参数也传不进来，比如上面定义的静态变量
+                    System.out.println("---------"+ss);
+                    // 但是在此处可以处理业务，可以有返回值
+                    return "jhahahha";
+                });
+        System.out.println(ft1.join());
         TimeUnit.SECONDS.sleep(100L);
 
 

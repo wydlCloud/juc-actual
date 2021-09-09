6ed7315cd972cfeb63c2c6dc646116b2fae5e721
yunze <yunze.wydl@raycloud.com>
2021-09-09 20:28:50 +0800

    API

--- src/main/java/com/wy/learn/completeFuture/api/CompletableFutureApi.java
+++ src/main/java/com/wy/learn/completeFuture/api/CompletableFutureApi.java
@@ -13,26 +13,105 @@ public class CompletableFutureApi {
 
 
     public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
-//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
-//        Integer join = CompletableFuture.supplyAsync(() -> {
-//            return 1;
-//        }).thenApply(s -> {
-//            return s + 2;
-//        }).whenComplete((v, e) -> {
-//            if (e == null) {
-//                System.out.println("zoulema");
-//                System.out.println(e);
-//            }
-//            System.out.println("zoulema---");
-//            System.out.println(v);// 这个V就是上一步的返回结果
-//        }).exceptionally(e -> {
-//            e.printStackTrace();
-//            return null;
-//        }).join();
-//
-//        System.out.println(join);
 //        m1();
-        m2();
+//        m2();
+//        m3();
+//        m4();
+//        m5();
+        m6();
+        m7();
+
+    }
+
+
+    public static void m7(){
+        CompletableFuture.supplyAsync(() -> {
+            return 1;
+        }).thenApply(f -> {
+            return f+2;
+        }).thenApply(f -> {
+            return f+3;
+        }).thenAccept(r -> System.out.println(r));
+
+
+        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenRun(() -> {}).join());
+
+
+        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenAccept(resultA -> {}).join());
+
+
+        System.out.println(CompletableFuture.supplyAsync(() -> "resultA").thenApply(resultA -> resultA + " resultB").join());
+    }
+    /**
+     * 对计算速度进行选用 也就是or的聚合，只要有一个线程完成，就可以完成下面的任务，也就是先执行完的先触发
+     * applyToEither
+     */
+    public static void m6() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        CompletableFuture<Integer> integerCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10L);
+                System.out.println("t1"+"异步线程执行结束");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        },threadPoolExecutor).applyToEither(CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3L);
+                System.out.println("t2异步线程执行结束");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 2;
+        }), r -> {
+            return r;
+        });
+
+        Integer join = integerCompletableFuture.join();
+        System.out.println(join);
+        System.out.println("main 线程结束 end");
+    }
+
+    /**
+     * and 汇聚运算  还可以有其他的写法，就是分开写，或者看泡茶的那个实例
+     */
+    public static void m5() {
+        CompletableFuture<Integer> thenCombine = CompletableFuture.supplyAsync(() -> {
+            return 10;
+        }).thenCombine(CompletableFuture.supplyAsync(() -> {
+            return 20;
+        }), (r1, r2) -> {
+            return r1 + r2;
+        });
+        Integer join = thenCombine.join();
+        System.out.println(join);
+
+    }
+
+    /**
+     * thenAppay是使用异步线程的那个线程来进行回调以及执行，不会用新的线程来去做。
+     * 异步结果回调处理，减少future的阻塞和轮询空转cpu，执行获取结构后，会进行回调方法
+     */
+    public static void m4() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        Integer join = CompletableFuture.supplyAsync(() -> {
+            return 1;
+        },threadPoolExecutor).thenApply(s -> {
+            return s + 2;
+        }).whenComplete((v, e) -> {
+            if (e == null) {
+                System.out.println("zoulema");
+                System.out.println(e);
+            }
+            System.out.println("zoulema---");
+            System.out.println(v);// 这个V就是上一步的返回结果
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            return null;
+        }).join();
+
+        System.out.println(join);
     }
 
     /**
@@ -62,6 +141,7 @@ public class CompletableFutureApi {
         System.out.println(cf.complete(-44) + "\t" + cf.get());
     }
 
+    // 对结果进行回调，有入参进行消费，并且有返回值
     public static void m2() throws InterruptedException {
         CompletableFuture<Integer> cf = CompletableFuture.supplyAsync(() -> {
             try {
@@ -94,6 +174,38 @@ public class CompletableFutureApi {
         }
         // 此处主线程先不要结束，否则completableFuture默认线程池会立刻关闭
         TimeUnit.SECONDS.sleep(20);
+    }
 
+    // 对计算结果进行处理，并且有返回值，有异常给出来
+    public static void m3() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 20, 1L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        CompletableFuture<Integer> m3 = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 1;
+        }, threadPoolExecutor).handle((h, e) -> {
+            System.out.println("-----" + 2);
+            return h + 2;
+        }).handle((h, e) -> {
+            System.out.println("-------" + 3);
+            return h + 3;
+        }).handle((h, e) -> {
+            System.out.println("--------" + 4);
+            return h + 4;
+        }).whenComplete((v, e) -> {
+            if (e == null) {
+                System.out.println("------result" + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            return null;
+        });
+//        Integer join = m3.join();
+//        System.out.println(join);
+        System.out.println("主线程结束，异步线程就运行你自己的完吧，我不需要，但是需要使用自定义线程池," +
+                "如果不自定义，就会导致默认线程池会伴随着线程结束而释放，不管执行是否结束与否");
     }
 }
